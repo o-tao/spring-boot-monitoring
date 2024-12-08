889951b1971b0d9b0e7e1dc8d289ed6630e2f08d
o-tao <dhtkdals369@gmail.com>
2024-12-09 00:54:25 +0900

    프로젝트 생성

---
+++ src/test/java/monitoring/SpringBootMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
